3856bdfaca1758f5b33e7ba1f91e5742741436ae
michelle12369 <p5i7gaaaa@gmail.com>
2016-09-29 16:47:13 +0800

    社群前端功能完全完成

--- app/assets/javascripts/user/home.js
+++ app/assets/javascripts/user/home.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
-	$('.nav-search-glass').click(function(){
-		$('.search-friend').slideToggle("fast");
-	});
+	// $('.nav-search-glass').click(function(){
+	// 	$('.search-friend').slideToggle("fast");
+	// });
 
-	$('.alert-notice,.alert-alert').fadeIn(1000).delay(4000).fadeOut(1000);
+	// $('.alert-notice,.alert-alert').fadeIn(1000).delay(4000).fadeOut(1000);
 
 });
