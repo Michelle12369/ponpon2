ba08a1759d6508c42c7acfbb8acd1b109c83b6ec
michelle12369 <p5i7gaaaa@gmail.com>
2016-10-27 23:54:02 +0800

    try to fix pic on heroku

--- app/uploaders/crop_uploader.rb
+++ app/uploaders/crop_uploader.rb
@@ -7,7 +7,7 @@ class CropUploader < CarrierWave::Uploader::Base
     include Cloudinary::CarrierWave
   end
   # Choose what kind of storage to use for this uploader:
-  #storage :file
+  storage :file
   #storage :fog
 
   # Override the directory where uploaded files will be stored.
