a590b62f4e3cb6c9fd29c7b01fecbc79a99c6576
michelle12369 <p5i7gaaaa@gmail.com>
2016-11-01 11:37:29 +0800

    try to fix pic on heroku

--- app/uploaders/crop_uploader.rb
+++ app/uploaders/crop_uploader.rb
@@ -7,13 +7,15 @@ class CropUploader < CarrierWave::Uploader::Base
     include Cloudinary::CarrierWave
   end
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.development?
+    storage :file
   #storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    def store_dir
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   end
 
   def content_type_whitelist
