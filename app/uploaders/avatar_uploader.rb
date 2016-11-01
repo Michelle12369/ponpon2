364f37e6aee13929b54bf762a25bb04c681924c5
michelle12369 <p5i7gaaaa@gmail.com>
2016-11-01 11:28:14 +0800

    try to fix pic on heroku

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,20 +3,20 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-   include CarrierWave::RMagick
+   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
   if Rails.env.production?
     include Cloudinary::CarrierWave
   end
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   #storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
   def content_type_whitelist
     /image\//
