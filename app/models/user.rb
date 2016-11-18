68115e37392295bbed6b0f162a0514332941c1f2
michelle12369 <p5i7gaaaa@gmail.com>
2016-11-18 11:15:49 +0800

    fix fb small pic

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,7 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
-      user.remote_avatar_url = auth.info.image.gsub('http://','https://') # assuming the user model has an image
+      user.remote_avatar_url = auth.info.image.gsub('http://','https://')+ "?type=large" # assuming the user model has an image
       user.provider = auth.provider
       user.uid = auth.uid
       user.gender=auth.extra.raw_info.gender
