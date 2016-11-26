fc580955c7be11e227499c67193980e8cdee5345
leo <aleoliu566@gmail.com>
2016-11-26 15:49:58 +0800

    little change

--- app/helpers/coupons_helper.rb
+++ app/helpers/coupons_helper.rb
@@ -12,6 +12,21 @@ module CouponsHelper
 			end
 		end	
 	end
+	
+	def distributor2(coupon)
+		if coupon.root?||coupon.parent.root?
+			concat image_tag "store.png" , class: 'distributor_store'
+		elsif coupon.parent.user.nil?	
+			"已被刪除的使用者"
+		else
+			link_to user_path(coupon.parent.user) do
+				concat image_tag coupon.parent.user.avatar.url||"avatar.jpg", class: 'avatar coupon-profile-pic'
+				concat " "
+				concat coupon.parent.user.name
+			end
+		end	
+	end
+
 	def coupon_status(coupon)
 		if coupon.used==true
 			"已使用"
