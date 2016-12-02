7693badcb112cb9c2a040a975f4d81018fd57a6c
leo <aleoliu566@gmail.com>
2016-12-02 15:05:36 +0800

    console.log增加網址

--- app/assets/javascripts/user/home.js
+++ app/assets/javascripts/user/home.js
@@ -6,5 +6,6 @@ $(document).ready(function(){
 
 	// $('.alert-notice,.alert-alert').fadeIn(1000).delay(4000).fadeOut(1000);
 	console.log("%c請前往Qoupon粉絲專頁按讚",'color:red; font-size:40px;');
-	console.log("%c感謝您的支持＾＿",'color:red; font-size:20px;');
+	console.log("%chttps://goo.gl/FVhxAW",'color:grey; font-size:16px;');
+	console.log("%c感謝您的支持＾＿＾",'color:red; font-size:20px;');
 });
