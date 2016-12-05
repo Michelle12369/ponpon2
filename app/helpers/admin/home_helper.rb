0615be123df9f3b230b41a7564ff80d397763c18
michelle12369 <p5i7gaaaa@gmail.com>
2016-12-06 00:42:31 +0800

    coupon analysis

--- app/helpers/admin/home_helper.rb
+++ app/helpers/admin/home_helper.rb
@@ -1,7 +1,7 @@
 module Admin::HomeHelper
 	def calculate_rate(coupon)
 		if coupon.descendants.where("used=?",true).size!=0
-			number_to_percentage((coupon.descendants.where("used=?",true).size.to_f/coupon.descendants.size.to_f)*10)
+			number_to_percentage((coupon.descendants.where("used=?",true).size.to_f/coupon.descendants.size.to_f)*100)
 		else
 			number_to_percentage(0)
 		end
