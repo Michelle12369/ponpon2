a4841d5a60fb5d946ada7d037fc705e8cd1672f9
leo <aleoliu566@gmail.com>
2016-12-12 01:07:27 +0800

    修改後臺頁面：admin-landing page、admin、優惠券內容

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -8,3 +8,25 @@
 //= require bindWithDelay
 //= require ./admin/base
 //= require ./user/posts
+
+	var window_w = $(window).width();
+	var h = $('.fade1').height();
+	if( window_w > 560 ){
+		$(window).scroll(function() {
+			if($(window).scrollTop()> 300){
+				// $(".fade1").fadeTo( 1000 , 1);
+				$('.fade1').animate({ marginLeft : "-5%", opacity: 1 }, 1500);
+			}
+			if($(window).scrollTop()> (300 + 2 * h) ){
+				// $(".fade1").fadeTo( 1000 , 1);
+				$('.fade2').animate({ marginRight : "-5%", opacity: 1 }, 1500);
+			}
+			if($(window).scrollTop()> 300 + 3 * h){
+				// $(".fade1").fadeTo( 1000 , 1);
+				$('.fade3').animate({ marginLeft : "-5%", opacity: 1 }, 1500);
+			}
+		});
+	}
+	if( window_w <= 560 ){
+		$('.fade').css('opacity',1);
+	}
