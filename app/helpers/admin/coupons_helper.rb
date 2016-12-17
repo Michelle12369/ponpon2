639b66876c9db739fd0056efe94712ccea83b733
michelle12369 <p5i7gaaaa@gmail.com>
2016-12-17 16:12:46 +0800

    fix admin index

--- app/helpers/admin/coupons_helper.rb
+++ app/helpers/admin/coupons_helper.rb
@@ -8,7 +8,8 @@ module Admin::CouponsHelper
 	end
 	def send_or_delete(admin_coupon)
 		if admin_coupon.expiry_date<Date.today
-			link_to '刪除', admin_store_coupon_path(admin_coupon.store,admin_coupon), method: :delete, data: { confirm: '是否確認刪除此張優惠卷?' },class:"coupon-control-table-button-delete btn-use"
+			#link_to '刪除', admin_store_coupon_path(admin_coupon.store,admin_coupon), method: :delete, data: { confirm: '是否確認刪除此張優惠卷?' },class:"coupon-control-table-button-delete btn-use"
+			"已過期無法發送"
 		elsif admin_coupon.admin_coupon_limit>admin_coupon.descendants.size
 			link_to "發送", new_admin_store_coupon_search_path(admin_coupon.store,admin_coupon),class:"coupon-control-table-button-send"
 		else
