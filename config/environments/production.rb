fd5611ac63b10f9ab59b18ffc719ec099ff7337a
michelle12369 <p5i7gaaaa@gmail.com>
2016-12-18 22:04:03 +0800

    fix crop

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,7 @@ Rails.application.configure do
 
   #production 的 compile js
   config.assets.precompile += %w( coupons.coffee )
+   config.assets.precompile += %w( stores.coffee )
 
   #設定mailer
   config.action_mailer.default_url_options = { :host => 'www.liveqoupon.com' }
